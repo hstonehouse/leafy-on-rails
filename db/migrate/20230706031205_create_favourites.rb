b2a88e172f4ee4f554dbffc7ef9441da949ef511
Helen <hln.stonehouse@gmail.com>
2023-07-06 13:27:27 +1000

    Added Favourites table
    to capture the plants that a user adds to their "favourites"

---
+++ db/migrate/20230706031205_create_favourites.rb
@@ -0,0 +1,10 @@
+class CreateFavourites < ActiveRecord::Migration[7.0]
+  def change
+    create_table :favourites do |t|
+      t.belongs_to :plants
+      t.belongs_to :users
+      
+      t.timestamps
+    end
+  end
+end
