5d8da1e2ab3aa8794dd28dd5cd23055a89913025
Helen <hln.stonehouse@gmail.com>
2023-12-11 19:25:12 +1100

    🔧 Fix formatting

--- leafy-on-rails/src/pages/home.jsx
+++ leafy-on-rails/src/pages/home.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 import '../assets/css/home.css';
 
 export function Home() {
-    return (
-        <main>
-            <div className="homepage-container">
-                    <p id="heading">Leafy</p>
-                    <div id="logo-div">
-                        <FontAwesomeIcon icon="seedling" size="3x" color="rgb(17,136,60)" id="logo-image"/>
-                    </div>
-                    <Link to="/plantdirectory" className="button is-rounded" id="enter-button">Enter</Link>
-            </div>
-        </main>
-      );
+  return (
+    <main>
+      <div className="homepage-container">
+        <p id="heading">Leafy</p>
+        <div id="logo-div">
+          <FontAwesomeIcon icon="seedling" size="3x" color="rgb(17,136,60)" id="logo-image"/>
+        </div>
+        <Link to="/plantdirectory" className="button is-rounded" id="enter-button">Enter</Link>
+      </div>
+    </main>
+  );
 }
