675be7e9ee5f6b29842787999fa7df0d4fbc0653
Helen <hln.stonehouse@gmail.com>
2023-12-11 19:44:40 +1100

    🗽 Create placeholders for MyPlants and Plant pages

---
+++ leafy-on-rails/src/pages/myplants.jsx
@@ -0,0 +1,7 @@
+export function MyPlants() {
+  return (
+    <div>
+      <h1>My Plants</h1>
+    </div>
+  )
+}
---
+++ leafy-on-rails/src/pages/plant.jsx
@@ -0,0 +1,7 @@
+export function PlantPage() {
+  return (
+    <div>
+      <h1>Plant Page</h1>
+    </div>
+  )
+}
