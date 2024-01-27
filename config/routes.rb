7747b1d3ac078e6e8d1c571700e05bab03475401
Helen <hln.stonehouse@gmail.com>
2024-01-27 18:07:17 +1100

    ⬆️ Updated cors code to be less hacky

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '*all' => 'application#cors_preflight_check', :constraints => { method: 'OPTIONS' }
-
   get 'home/index'
   get 'api/plantdirectory', to: 'plant#all'
+  get 'api/plantsearch/:query', to: 'plant#search'
 end
