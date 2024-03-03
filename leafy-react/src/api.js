f9c4f6808c1b9cb30b18552278aa2a5c1e3a9456
Helen <hln.stonehouse@gmail.com>
2024-03-03 15:46:58 +1100

    🔧 Fix indentation

--- leafy-react/src/api.js
+++ leafy-react/src/api.js
@@ -3,19 +3,19 @@ const base_url = "http://localhost:3000/api";
 class api {
   async getAllPlants() {
     try {
-        const response = await fetch(`${base_url}/plantdirectory`);
-        return response.json();
+      const response = await fetch(`${base_url}/plantdirectory`);
+      return response.json();
     } catch (error) {
-        console.log("error", error)
+      console.log("error", error)
     }
   }
 
   async getOnePlant(id) {
     try {
-        const response = await fetch(`${base_url}/plant/${id}`);
-        return response.json();
+      const response = await fetch(`${base_url}/plant/${id}`);
+      return response.json();
     } catch (error) {
-        console.log("error", error)
+      console.log("error", error)
     }
   }
 
@@ -30,22 +30,22 @@ class api {
 
   async searchPlant(query) {
     try {
-        const response = await fetch(`${base_url}/plantsearch/${query}`);
-        const plant = await response.json();
-        console.log(plant)
-        return plant;
+      const response = await fetch(`${base_url}/plantsearch/${query}`);
+      const plant = await response.json();
+      console.log(plant)
+      return plant;
     } catch (error) {
-        console.log("error", error)
+      console.log("error", error)
     }
   }
 
   async getMyPlants(id) {
     try {
-        const response = await fetch(`${base_url}/${id}/myplants`);
-        const myPlants = await response.json();
-        return myPlants;
+      const response = await fetch(`${base_url}/${id}/myplants`);
+      const myPlants = await response.json();
+      return myPlants;
     } catch (error) {
-        console.log("error", error)
+      console.log("error", error)
     }
   }
 }
