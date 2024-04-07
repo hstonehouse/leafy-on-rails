92d23d6a6cba6516d03c311d40909a6dce53781f
Helen <hln.stonehouse@gmail.com>
2024-04-07 16:35:48 +1000

    🪓 Remove unneeded alias code

--- app/controllers/alias_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AliasController < ApplicationController
-  def plant_aliases
-    @aliases = Alias.where(plant_id: params[:id])
-    render json: @aliases
-  end
-end
